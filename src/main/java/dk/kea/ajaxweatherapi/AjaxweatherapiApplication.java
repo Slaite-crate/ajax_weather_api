b7c2f6101487de04fddf0c3ae3b361e85f720b36
Pelle <pelle.park@gmail.com>
2020-09-17 13:28:00 +0200

    hello

---
+++ src/main/java/dk/kea/ajaxweatherapi/AjaxweatherapiApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.ajaxweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxweatherapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxweatherapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/ajaxweatherapi/AjaxweatherapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.ajaxweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxweatherapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
